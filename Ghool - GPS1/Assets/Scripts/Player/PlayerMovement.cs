07934db9c6e7e4ead061476761bdc93e701cb5df
AstrixZ <0132554@kdu-online.com>
2023-02-20 16:30:13 +0800

    Added EnemySpawner & BuffSystem Script
    UNFINISHED.

--- Ghool - GPS1/Assets/Scripts/Player/PlayerMovement.cs
+++ Ghool - GPS1/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerStats playerStats; //needed to transfer speed value to PlayerStats Script
+
     public float movementspeed= 5f;
     public Rigidbody2D rb;
     Vector2 movement;
-    PlayerHP playerhp;
+    PlayerStats playerhp;
     private void Start()
     {
-       
+        playerStats.GetSpeed(movementspeed); //fuction to transfer
     }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(movement.x, movement.y);
+
+        transform.position = transform.position + (Vector3)(direction * movementspeed * Time.deltaTime);
+
         movementspeedthreshold();
+
     }
 
     void FixedUpdate()
@@ -27,19 +35,19 @@ public class PlayerMovement : MonoBehaviour
 
     public void movementspeedthreshold()
     {
-        if (GetComponent<PlayerHP>().currentHealth >= 80 && GetComponent<PlayerHP>().currentHealth <= 100)
+        if (GetComponent<PlayerStats>().currentHealth >= 80 && GetComponent<PlayerStats>().currentHealth <= 100)
         {
             movementspeed = 5f;
         }
-        else if (GetComponent<PlayerHP>().currentHealth >= 60 && GetComponent<PlayerHP>().currentHealth <= 79)
+        else if (GetComponent<PlayerStats>().currentHealth >= 60 && GetComponent<PlayerStats>().currentHealth <= 79)
         {
             movementspeed = 10f;
         }
-        else if (GetComponent<PlayerHP>().currentHealth >= 40 && GetComponent<PlayerHP>().currentHealth <= 59)
+        else if (GetComponent<PlayerStats>().currentHealth >= 40 && GetComponent<PlayerStats>().currentHealth <= 59)
         {
             movementspeed = 15f;
         }
-        else if (GetComponent<PlayerHP>().currentHealth >= 20 && GetComponent<PlayerHP>().currentHealth <= 39)
+        else if (GetComponent<PlayerStats>().currentHealth >= 20 && GetComponent<PlayerStats>().currentHealth <= 39)
         {
             movementspeed = 20f;
         }
